7d9cae1f0ef8729ff52ff12f8026b524d52d69f1
Sinu Twentyone <sinutwentyone@gmail.com>
2022-07-27 04:59:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   alert("ASU")
     fetch("https://shopee.co.id/api/v4/product/get_shop_info?shopid=44240271", { 
-      mode: 'no-cors'
+      mode: 'cors'
     })
     .then( ( response ) => console.log( response) )        
      .catch( ( response ) => console.log( response ) )
